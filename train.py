cd2e7c360ddb90534053f213103c67ec0f915cab
Ahmet Narman <ahmet.narman18@imperial.ac.uk>
2019-11-11 17:48:39 +0000

    Added a data saver function to save the generated images

--- train.py
+++ train.py
@@ -7,7 +7,11 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torchvision.transforms.functional as TF
+import os
+import shutil
 from PIL import Image
+
+
 
 Cuda = torch.cuda.is_available()
 
@@ -39,6 +43,8 @@ class SatelliteDataset(torch.utils.data.Dataset):
         
         self.testData, self.testLabels = self.getTestingData()
         
+        self.saveDataset(override=True)
+        
     def __getitem__(self, idx):
         """Returns the image and label of specified index from the training dataset"""
         
@@ -54,7 +60,6 @@ class SatelliteDataset(torch.utils.data.Dataset):
             if extrema == (0,0):
                 blacklist.append(i)
         
-        print(blacklist)
         images  = [images[i] for i in range(len(images)) if i not in blacklist]
         labels  = [labels[i] for i in range(len(labels)) if i not in blacklist]
         
@@ -72,17 +77,37 @@ class SatelliteDataset(torch.utils.data.Dataset):
     def augmentData(self, images, labels):
         # First step of the augmentation is rotating the images in different angles
         # The buildings on the dataset can have different orientations so this augmentation is very useful
-
-        for i in range(len(images)): # For loops are done over indexes so transformation can be done on both images and labels
-            print(i)
-            for ang in [1,2,3,4]*90: # 6 rotation angles seperated by 60 degrees
-                images.append(TF.rotate(images[i], ang, resample = Image.BILINEAR))
-                labels.append(TF.rotate(labels[i], ang, resample = Image.BILINEAR))
-
+        # Each augmentation type is done on the dataset extended by the previous augmentation
         
+        # Images rotated by intervals of 90 degrees (Quadruples the data)
+        # More frequent rotations could be done as well but 
+        for i in range(len(images)): # For loops are done over indexes so transformation can be done on both images and labels
+            for ang in range(90,360,90): # zero degrees is not included as it is already in the array
+                images.append(TF.rotate(images[i], ang, resample = Image.BILINEAR, expand=True))
+                labels.append(TF.rotate(labels[i], ang, resample = Image.BILINEAR, expand=True))
+        
+        # Images flipped horizontally (Doubles the data)
+        for i in range(len(images)):
+            images.append(TF.hflip(images[i]))
+            labels.append(TF.hflip(labels[i]))
+            
+        # Images flipped vertically (Doubles the data)        
+        for i in range(len(images)):
+            images.append(TF.vflip(images[i]))
+            labels.append(TF.vflip(labels[i]))
+
+        # Brightness and contrast adjusted images added (Triples the data)
+        # They could be adjusted separately but that would make the dataset too big
+        for i in range(len(images)):
+            images.append(TF.adjust_contrast(TF.adjust_brightness(images[i], brightness_factor=1.2), contrast_factor=1.2))
+            labels.append(labels[i]) # Label brightness does not change
+            images.append(TF.adjust_contrast(TF.adjust_brightness(images[i], brightness_factor=0.8), contrast_factor=0.8))
+            labels.append(labels[i]) # Label brightness does not change
+              
         return images, labels
     
     def sliceRaw(self, mode):
+        """Slices the raw data (image and ground truth) into 256*256 pieces"""
         if mode == 'test':  
             # For testing data, only the images that are just sliced from the raw image are used
             images = []
@@ -105,6 +130,44 @@ class SatelliteDataset(torch.utils.data.Dataset):
                     images.append(TF.crop(self.rawData, j*temp, i*temp, temp*2, temp*2))
                     labels.append(TF.crop(self.rawLabels, j*temp, i*temp, temp*2, temp*2))         
             return images, labels
+
+    def saveDataset(self, override=False):
+        """
+        Saves the images and labels extracted from the raw images to local files
+        
+        """
+        if not os.path.isdir('dataset') or override:
+            
+            if os.path.isdir('dataset'):
+                shutil.rmtree('dataset') # shutil is used to remove a nonempty directory
+            os.mkdir('dataset')
+            
+            os.chdir('dataset')
+            os.mkdir('train')
+            os.mkdir('test')
+            
+            os.chdir('train')
+            os.mkdir('images')
+            os.mkdir('labels')
+  
+            os.chdir('..')
+            os.chdir('test')     
+            os.mkdir('images')
+            os.mkdir('labels')
+            
+            os.chdir('..')
+            os.chdir('..')
+            
+            for i in range(len(self.trainData)):
+                self.trainData[i].save(os.path.join('dataset', 'train', 'images', 'im'+str(i)+'.png'))
+                self.trainLabels[i].save(os.path.join('dataset', 'train', 'labels', 'gt'+str(i)+'.png'))
+            
+            for i in range(len(self.testData)):
+                self.testData[i].save(os.path.join('dataset', 'test', 'images', 'im'+str(i)+'.png'))
+                self.testLabels[i].save(os.path.join('dataset', 'test', 'labels', 'gt'+str(i)+'.png'))
+        else:
+            print('The dataset is already created. To save a new one, use the override option')
+
             
 class SatelliteDataLoader(torch.utils.data.DataLoader):
     
@@ -176,13 +239,13 @@ for i in range(8):
     plt.imshow(trainIms[rInd])
     plt.subplot(4,8,i+25)
     plt.imshow(trainLabels[rInd])
-
+plt.show()
 
 
 # It was found that the ground truth contains two values for every pixel: 
 # - (255,255,255): White segments in the ground truth image
 # - (0,0,0): Black segments in the ground truth image
-"""
+
 plt.figure(figsize=(13,6))
 plt.subplot(1,2,1)
 plt.imshow(dataset.rawData)
@@ -190,7 +253,7 @@ plt.title('Input image')
 plt.subplot(1,2,2)
 plt.imshow(dataset.rawLabels)
 plt.title('Ground truth labels')
-"""
+
 
 #%% Data preprocessing
 
